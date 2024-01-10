9720460d5413a6184e0a2c69c00cacd4f1860dd0
Steven Gonzalez <srgm86@outlook.com>
2024-01-10 15:10:01 -0500

    migrated to vite

--- src/models/story.ts
+++
@@ -1,9 +0,0 @@
-export type StoryModel = {
-    userName: string,
-    image: string,
-    description: string,
-    comments: number,
-    likes : number,
-}
-
-export type StoriesModel = StoryModel[];
